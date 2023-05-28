f663ff30f3b54e53062440114d5db76df2240244
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-05-28 21:49:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
+  'live_Yev63kKIKQlvuwfV91tmMhA4RcWdQRKBtpQEc40LBA3Gqtc0uo9ZdRzJSAOtGlda';
+const BREEDS_IDS = 'breed_ids';
+
 const refs = {
   select: document.querySelector('.breed-select'),
   loader: document.querySelector('.loader'),
@@ -6,10 +11,6 @@ const refs = {
 };
 
 function fetchBreeds() {
-  const BASE_URL = 'https://api.thecatapi.com/v1';
-  const API_KEY =
-    'live_Yev63kKIKQlvuwfV91tmMhA4RcWdQRKBtpQEc40LBA3Gqtc0uo9ZdRzJSAOtGlda';
-
   return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
     if (!response.ok) {
       throw new Error('response.statusText');
@@ -33,3 +34,21 @@ function renderCats(cats) {
   refs.select.insertAdjacentHTML('beforeend', marcup);
 }
 
+
+
+refs.select.addEventListener('change', fetchCatByBreed);
+
+function fetchCatByBreed(breedId) {
+  // console.log(breedId)
+
+  return fetch(
+    `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error('response.statusText');
+    }
+    return response.json();
+  });
+}
+
+fetchCatByBreed().then(data => console.log(data)).catch(error => console.log(error))
