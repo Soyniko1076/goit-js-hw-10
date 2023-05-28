9df01a20568c79ccd662b94e240709a963a4be5e
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-05-28 20:29:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,15 +5,10 @@ const refs = {
   catInfo: document.querySelector('.cat-info'),
 };
 
-const select = document.querySelector('.breed-select');
-const loader = document.querySelector('.loader');
-
 function fetchBreeds() {
   const BASE_URL = 'https://api.thecatapi.com/v1';
   const API_KEY =
     'live_Yev63kKIKQlvuwfV91tmMhA4RcWdQRKBtpQEc40LBA3Gqtc0uo9ZdRzJSAOtGlda';
-
-  // console.log(select)
 
   return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
     if (!response.ok) {
@@ -25,8 +20,6 @@ function fetchBreeds() {
 
 fetchBreeds()
   .then(cats => {
-    // console.log(cats);
-
     renderCats(cats);
   })
   .catch(error => console.log(error));
@@ -37,7 +30,6 @@ function renderCats(cats) {
       return `<option value=${id}>${name}</option>`;
     })
     .join('');
-  //   console.log(marcup);
-
   refs.select.insertAdjacentHTML('beforeend', marcup);
 }
+
