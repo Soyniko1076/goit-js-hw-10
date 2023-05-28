cb8cbe369bcce01fe59bcf434a24e537e7b097dc
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-05-28 19:31:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
+  'live_Yev63kKIKQlvuwfV91tmMhA4RcWdQRKBtpQEc40LBA3Gqtc0uo9ZdRzJSAOtGlda';
+
+const select = document.querySelector(".breed-select");
+// console.log(select)
 function fetchBreeds() {
-  const BASE_URL = 'https://api.thecatapi.com/v1';
-  const API_KEY =
-        'live_Yev63kKIKQlvuwfV91tmMhA4RcWdQRKBtpQEc40LBA3Gqtc0uo9ZdRzJSAOtGlda';
-    
-    return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}&`).then(resp => {
-        if (!resp.ok) {
-        throw new Error('resp.statusText')
-        }
-        return resp.json()
-    })
+  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error('response.statusText');
+    }
+    return response.json();
+  });
 }
 
-fetchBreeds().then(data => console.log(data)).catch(err => console.log (err))
+fetchBreeds()
+  .then(data => {
+    // console.log(data);
+    renderCats(data);
+  })
+  .catch(error => console.log(error));
+
+
+function renderCats(data) {
+  const marcup = data
+    .map(({ id, name }) => {
+      return `<option value=${id}>${name}</option>`;
+    })
+    .join('');
+//   console.log(marcup);
+  select.insertAdjacentHTML('beforeend', marcup);
+}
+
+
