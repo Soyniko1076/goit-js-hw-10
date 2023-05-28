f9cc61d0d00dbc75c1b072186b96c641d76e0b03
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-05-28 20:26:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const refs = {
-select: document.querySelector(".breed-select"),
-loader: document.querySelector(".loader")
-}
+  select: document.querySelector('.breed-select'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+  catInfo: document.querySelector('.cat-info'),
+};
 
-const select = document.querySelector(".breed-select");
-const loader = document.querySelector(".loader");
-
+const select = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
 
 function fetchBreeds() {
+  const BASE_URL = 'https://api.thecatapi.com/v1';
+  const API_KEY =
+    'live_Yev63kKIKQlvuwfV91tmMhA4RcWdQRKBtpQEc40LBA3Gqtc0uo9ZdRzJSAOtGlda';
 
-const BASE_URL = 'https://api.thecatapi.com/v1';
-const API_KEY =
-  'live_Yev63kKIKQlvuwfV91tmMhA4RcWdQRKBtpQEc40LBA3Gqtc0uo9ZdRzJSAOtGlda';
-
-// console.log(select)
+  // console.log(select)
 
   return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
     if (!response.ok) {
@@ -31,16 +31,13 @@ fetchBreeds()
   })
   .catch(error => console.log(error));
 
-
 function renderCats(cats) {
   const marcup = cats
     .map(({ id, name }) => {
       return `<option value=${id}>${name}</option>`;
     })
     .join('');
-//   console.log(marcup);
-  
+  //   console.log(marcup);
+
   refs.select.insertAdjacentHTML('beforeend', marcup);
 }
-
-
