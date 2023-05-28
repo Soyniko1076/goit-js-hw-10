d7a55466d64cf73dcb7a9ed8409571527bcf48df
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-05-28 16:31:49 +0000

    Deploying to gh-pages from @ Soyniko1076/goit-js-hw-10@cb8cbe369bcce01fe59bcf434a24e537e7b097dc 🚀

--- index.3cbbc5e4.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.thecatapi.com/v1/breeds?api_key=live_Yev63kKIKQlvuwfV91tmMhA4RcWdQRKBtpQEc40LBA3Gqtc0uo9ZdRzJSAOtGlda&").then((t=>{if(!t.ok)throw new Error("resp.statusText");return t.json()})).then((t=>console.log(t))).catch((t=>console.log(t)));
-//# sourceMappingURL=index.3cbbc5e4.js.map
--- index.972d659c.js
+++
@@ -1,2 +0,0 @@
-fetch("".concat("https://api.thecatapi.com/v1","/breeds?api_key=").concat("live_Yev63kKIKQlvuwfV91tmMhA4RcWdQRKBtpQEc40LBA3Gqtc0uo9ZdRzJSAOtGlda","&")).then((function(t){if(!t.ok)throw new Error("resp.statusText");return t.json()})).then((function(t){return console.log(t)})).catch((function(t){return console.log(t)}));
-//# sourceMappingURL=index.972d659c.js.map
